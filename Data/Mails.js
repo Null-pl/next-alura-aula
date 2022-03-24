d6a4cf0c3d0fcabbfd7993edc0b99a5afc590d57
Pedro Lucas Macedo de Moraes <73931509+PedroLucas-M-m@users.noreply.github.com>
2022-03-24 13:26:34 -0300

    update

---
+++ Data/Mails.js
@@ -0,0 +1,50 @@
+export const Mails = [
+    {
+        1 : {
+            id :  1,
+            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
+            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png",
+            mobile: "https://next-alura-aula.vercel.app/Api/htmls/html_02.png"
+        },
+        2 : {
+            id :  2,
+            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
+            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png",
+            mobile: "https://next-alura-aula.vercel.app/Api/htmls/html_02.png"
+        },
+        3 : {
+            id :  3,
+            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
+            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png",
+            mobile: "https://next-alura-aula.vercel.app/Api/htmls/html_02.png"
+        },
+        4 :{
+            id :  4,
+            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
+            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png",
+            mobile: "https://next-alura-aula.vercel.app/Api/htmls/html_02.png"
+        }
+    },
+    {
+        1 : {
+            id :  1,
+            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
+            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png"
+        },
+        2 : {
+            id :  2,
+            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
+            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png"
+        },
+        3 : {
+            id :  3,
+            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
+            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png"
+        },
+        4 :{
+            id :  4,
+            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
+            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png"
+        }
+    }
+]
--- pages/api/Emails.js
+++ pages/api/Emails.js
@@ -1,27 +1,8 @@
-function Emials(request, response){
+import { Mails } from "../../Data/Mails"
 
-    response.json([
-        {
-            id :  1,
-            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
-            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png"
-        },
-        {
-            id :  2,
-            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
-            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png"
-        },
-        {
-            id :  3,
-            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
-            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png"
-        },
-        {
-            id :  4,
-            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
-            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png"
-        }
-    ])
+function Emials(request, response){
+    
+    response.json(Mails)
 
 }
 
