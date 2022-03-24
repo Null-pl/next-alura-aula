36ffd438e141b4a782b9d0118b10cf6aeb32600a
Pedro Lucas Macedo de Moraes <73931509+PedroLucas-M-m@users.noreply.github.com>
2022-03-24 11:57:03 -0300

    Create Emails.js

---
+++ pages/api/Emails.js
@@ -0,0 +1,28 @@
+function Emials(request, response){
+
+    response.json([
+        {
+            id :  1,
+            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
+            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png"
+        },
+        {
+            id :  2,
+            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
+            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png"
+        },
+        {
+            id :  3,
+            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
+            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png"
+        },
+        {
+            id :  4,
+            banner : "https://next-alura-aula.vercel.app/Api/banners/banner_01.png",
+            html : "https://next-alura-aula.vercel.app/Api/htmls/html_01.png"
+        }
+    ])
+
+}
+
+export default Emials
